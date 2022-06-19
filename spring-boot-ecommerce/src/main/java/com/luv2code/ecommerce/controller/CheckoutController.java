f6f0af0bd2abe7a011c1f8446e5e3b7331f658c5
amer <amouzaien.ameur@gmail.com>
2022-06-19 13:41:47 +0300

    paymemt intent

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -1,36 +1,49 @@
 package com.luv2code.ecommerce.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.service.CheckoutService;
-import org.springframework.web.bind.annotation.*;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 @CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
 
-    private CheckoutService checkoutService;
+	private CheckoutService checkoutService;
 
-    public CheckoutController(CheckoutService checkoutService) {
-        this.checkoutService = checkoutService;
-    }
+	public CheckoutController(CheckoutService checkoutService) {
+		this.checkoutService = checkoutService;
+	}
 
-    @PostMapping("/purchase")
-    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+	@PostMapping("/purchase")
+	public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
 
-        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+		PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
-        return purchaseResponse;
-    }
+		return purchaseResponse;
+	}
+
+	@PostMapping("/payment-intent")
+
+	public ResponseEntity<String> createpaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+		PaymentIntent paymentIntent = checkoutService.createpaymentIntent(paymentInfo);
+
+		String paymentStr = paymentIntent.toJson();
+
+		return new ResponseEntity<String>(paymentStr, HttpStatus.OK);
+
+	}
 
 }
-
-
-
-
-
-
-
-
-
